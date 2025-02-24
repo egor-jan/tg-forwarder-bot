3a2be263488b24950cdee6b6aba6e1dbb8be8d49
egor-jan <eeleskin.egr@gmail.com>
2025-02-24 13:22:37 +0300

    do not forward messages in group

--- src/bot/handlers/anyMessage.ts
+++ src/bot/handlers/anyMessage.ts
@@ -6,6 +6,8 @@ import { CONFIG } from '../../config';
 
 export class ForwardMessage implements IMessageHandler {
   public execute = async (ctx: Context<Update>): Promise<void> => {
+    if (CONFIG.chat.main_forward_id === ctx.chat?.id.toString()) return;
+
     await ctx.forwardMessage(CONFIG.chat.main_forward_id);
   };
 }
